cf83cea2307feef7acad95630cfedbe528f8fb0f
I.G <ingrida.gudaityte@gmail.com>
2022-05-14 11:41:06 +0300

    dinamic root pvz

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
+// const express = require('express');
+
+// const app = express();
+
+// app.listen(9000, () => {
+//     console.log('serveris paleistas!');
+// });
+
+// app.get('/', (request, response) => {
+//     response.send('labas');
+// });
+
+// 2022 05 14 pvz
+
 const express = require('express');
 
 const app = express();
 
 app.listen(9000, () => {
-    console.log('serveris paleistas!');
+    console.log(`Serveris paleistas. Laukia užklausų`);
 });
 
-app.get('/', (request, response) => {
-    response.send('labas');
+app.get('/barista/:gerimas/:kiekis', (request, response) => {
+    console.log(request.params);
+
+    const gerimoPavadinimas = request.params.gerimas;
+    const puodeliuKiekis = request.params.kiekis;
+
+    console.log('pasiimam puodeli');
+    console.log(`ipilam ${gerimoPavadinimas} i puodeli`);
+
+    response.json(`${puodeliuKiekis} puodelis ${gerimoPavadinimas}`);
+    console.log('atiduodam puodeli');
+
+    if (puodeliuKiekis > 1) {
+        console.log('tvarkom darbo vieta');
+        return;
+    }
+
+    console.log('laukiam sekancio kliento');
 });
