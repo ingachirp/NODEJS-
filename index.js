daf1b74a751dd85b853f6c520f56ae27ce2102f9
I.G <ingrida.gudaityte@gmail.com>
2022-05-14 12:57:53 +0300

    1 ir 2 uzduotis

--- index.js
+++ index.js
@@ -10,32 +10,29 @@
 //     response.send('labas');
 // });
 
-// 2022 05 14 pvz
+// 2022 05 14 uzduotis
 
+// 
 const express = require('express');
-
 const app = express();
 
-app.listen(9000, () => {
+const knygos = [
+    "Tadas Blinda",
+    "Zaidimu knyga",
+    "Chemijos zinynas"
+]
+
+app.listen(3000, () => {
     console.log(`Serveris paleistas. Laukia užklausų`);
 });
 
-app.get('/barista/:gerimas/:kiekis', (request, response) => {
-    console.log(request.params);
+app.get('/books', (request, response) => {
+    response.json(knygos);
+});
 
-    const gerimoPavadinimas = request.params.gerimas;
-    const puodeliuKiekis = request.params.kiekis;
-
-    console.log('pasiimam puodeli');
-    console.log(`ipilam ${gerimoPavadinimas} i puodeli`);
-
-    response.json(`${puodeliuKiekis} puodelis ${gerimoPavadinimas}`);
-    console.log('atiduodam puodeli');
-
-    if (puodeliuKiekis > 1) {
-        console.log('tvarkom darbo vieta');
-        return;
-    }
-
-    console.log('laukiam sekancio kliento');
+app.get('/books/:id', (request, response) => {
+    
+    const knygosPavadinimas = request.params.id;
+    response.json(knygos[request.params.id]);
+        
 });
