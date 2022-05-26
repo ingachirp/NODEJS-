8c5d5967f5dc375a8ffd72f96840019937df170f
I.G <ingrida.gudaityte@gmail.com>
2022-05-26 21:08:00 +0300

    darbas su MongoDB prijungti knyga

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-// const express = require('express');
+require('dotenv').config();
 
-// const app = express();
-
-// app.listen(9000, () => {
-//     console.log('serveris paleistas!');
-// });
-
-// app.get('/', (request, response) => {
-//     response.send('labas');
-// });
-
-// 2022 05 14 uzduotis
-
-//
 const express = require('express');
 
 const app = express();
 
-console.log(process.env);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = 'mongodb+srv://IngridaVIGI13:byuhblf77@cluster0.eipbj.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 const knygos = [
   'Tadas Blinda',
@@ -25,8 +19,41 @@ const knygos = [
   'Chemijos zinynas',
 ];
 
-app.listen(9000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Serveris paleistas. Laukia užklausų');
+});
+
+app.get('/books', (request, response) => {
+  client.connect(async () => {
+    const collection = client.db('knygu_projektas').collection('books');
+    const result = await collection.insertOne({
+      name: 'Haris poteris',
+      pageCount: 700,
+    });
+    console.log(result);
+    client.close();
+  });
+
+  response.json(knygos);
+});
+
+app.get('/books/:id', (request, response) => {
+  response.json(knygos[request.params.id]);
+});
+
+app.post('/books', (request, response) => {
+  const { bookName } = request.body.bookName;
+  const { bookPageCount } = request.body;
+
+  client.connect(async () => {
+    const collection = client.db('knygu_projektas').collection('books');
+    const result = await collection.insertOne({
+      name: request.book.bookName,
+      pageCount: request.body.bookPageCount,
+    });
+    console.log(result);
+    client.close();
+  });
 });
 
 app.get('/books/:from/:to', (request, response) => {
